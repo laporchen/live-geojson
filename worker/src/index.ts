69c13d2063bdca2096b247e619d079f3b50a0d23
Lapor Chen <31620109+laporchen@users.noreply.github.com>
2025-01-07 13:40:45 +0800

    feat: setup crossws

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,65 +1,33 @@
 import { DurableObject } from 'cloudflare:workers'
+import crossws from 'crossws/adapters/cloudflare-durable'
+import { createHandler } from 'y-crossws'
 
-/**
- * Welcome to Cloudflare Workers! This is your first Durable Objects application.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your Durable Object in action
- * - Run `npm run deploy` to publish your application
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/durable-objects
- */
+// Durable Object Name is forced unless we can fit service name prop in `crossws`
+// @ts-expect-error type error
+const ws = crossws(createHandler())
 
-/** A Durable Object's behavior is defined in an exported Javascript class */
-export class LiveGeoJsonObject extends DurableObject {
-	/**
-	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
-	 * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
-	 *
-	 * @param ctx - The interface for interacting with Durable Object state
-	 * @param env - The interface to reference bindings declared in wrangler.toml
-	 */
-	constructor(ctx: DurableObjectState, env: Env) {
-		super(ctx, env)
-	}
+export class $DurableObject extends DurableObject {
+	constructor(state: DurableObjectState, env: any) {
+    super(state, env);
+    ws.handleDurableInit(this, state, env)
+  }
 
-	/**
-	 * The Durable Object exposes an RPC method sayHello which will be invoked when when a Durable
-	 *  Object instance receives a request from a Worker via the same method invocation on the stub
-	 *
-	 * @param name - The name provided to a Durable Object instance from a Worker
-	 * @returns The greeting to be sent back to the Worker
-	 */
-	async sayHello(name: string): Promise<string> {
-		return `Hello, ${name}!`
-	}
+	fetch(request: Request) {
+    return ws.handleDurableUpgrade(this, request)
+  }
+  webSocketMessage(client: WebSocket, message: string) {
+    return ws.handleDurableMessage(this, client, message)
+  }
+  webSocketClose(client: WebSocket, code: number, reason: string, wasClean: boolean) {
+    return ws.handleDurableClose(this, client, code, reason, wasClean)
+  }
 }
 
 export default {
-	/**
-	 * This is the standard fetch handler for a Cloudflare Worker
-	 *
-	 * @param request - The request submitted to the Worker from the client
-	 * @param env - The interface to reference bindings declared in wrangler.toml
-	 * @param ctx - The execution context of the Worker
-	 * @returns The response to be sent back to the client
-	 */
-	async fetch(request, env, ctx): Promise<Response> {
-		// We will create a `DurableObjectId` using the pathname from the Worker request
-		// This id refers to a unique instance of our 'MyDurableObject' class above
-		let id: DurableObjectId = env.LIVE_GEOJSON_OBJECT.idFromName(new URL(request.url).pathname)
-
-		// This stub creates a communication channel with the Durable Object instance
-		// The Durable Object constructor will be invoked upon the first call for a given id
-		let stub = env.LIVE_GEOJSON_OBJECT.get(id)
-
-		// We call the `sayHello()` RPC method on the stub to invoke the method on the remote
-		// Durable Object instance
-		let greeting = await stub.sayHello('world')
-
-		return new Response(greeting)
-	},
+	async fetch(request: Request, env, context) {
+    if (request.headers.get('upgrade') === 'websocket') {
+      return ws.handleUpgrade(request as any, env, context)
+    }
+    return new Response('', { status: 426 })
+  }
 } satisfies ExportedHandler<Env>
